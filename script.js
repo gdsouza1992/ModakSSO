4926a52506a0b865e004366f695010c0d4fb88d1
Gareth Dsouza <gdsouza@usc.edu>
2016-11-18 14:45:42 -0800

    Added JS and functionality

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
+
 	var token = localStorage.getItem("lastname");
 	if(!token){
 		// Store
 		$("#serverMsg").text("Please use the redeem button when you are at the booth \n The coupon will last for 5 minutes only once redeemed");
-		$("#buttonDiv").removeClass(".hidden");
+		$("#buttonDiv").removeClass("hidden");
 	}else{
 		//Get
 		$("#serverMsg").text("Thank you for registering with MODAK! \n You have already used this coupon!");
-
+		$("#buttonDiv").addClass("hidden");
 	}
 
 	$("#redeem").click(function(){
